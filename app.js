8ccf6efa6dd80345600f3c9a9c26c5bc6e200bf1
Samantha Malone <sammac99@gmail.com>
2021-06-17 15:51:13 -0700

    capture user input

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var message = 'Hello Node!';
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
-var sum = 5 + 3;
+// create and call function
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
 
-console.log(message);
-console.log(sum);
+    console.log('================');
+
+  // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+  };
+  
+  printProfileData(profileDataArgs);
